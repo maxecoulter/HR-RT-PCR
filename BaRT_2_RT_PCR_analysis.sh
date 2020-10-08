5f377c6de2c74d4c665463c6e04829c554f18411
Max Coulter <47447084+maxecoulter@users.noreply.github.com>
2020-10-08 11:31:23 +0100

    Update BaRT_2_RT_PCR_analysis.sh

--- BaRT_2_RT_PCR_analysis.sh
+++ BaRT_2_RT_PCR_analysis.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -j yes
 #$ -pe smp 28
-#
+
 
 # Script to:
 # First generate a transcriptome DB ;
@@ -12,31 +12,19 @@
 
 
 ###Files names and paths:
-DataBaseDB="Transcriptome_DB2"
+DataBaseDB="Transcriptome_DB"
 
-#BlastN_Output="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/Primers_vs_AllTranscripts_BaRT_1p_12"
-#BlastN_Output="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/Primers_vs_AllTranscripts_BaRT_Illumina3_12"
-#BlastN_Output="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/Primers_vs_AllTranscripts_BaRT_Iso4_12"
-BlastN_Output="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/Primers_vs_AllTranscripts_BaRT_2_10_12"
+BlastN_Output="/path/to/Primers_vs_AllTranscripts"
 
+TranscriptomeFileName="/path/to/Transcriptome.fasta"
 
-#TranscriptomeFileName="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/transcriptomes/BaRT.1.0_Unsplit_exons_padded.fasta"
-TranscriptomeFileName="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/transcriptomes/BaRT_2.0.fasta"
+PrimersFileName="/path/to/primers.fasta" # the primer names must end with "R" (reverse) and "F" (forward) and must form pairs with the same name (example: >101R ; >101F)
 
+PathToSamplesFolder="/path/to/salmon_quants/"
 
+rtPCRInput="/path/to/rtPCR_productsAndProportions.txt"  ## ## Header: "Primer 	Size	INF2	LEAF	EMBRYO	INF1	NODE"
 
-PrimersFileName="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/primers_RT_PCR1_updated.fasta" # the primer names must end with "_R" (reverse) and "_F" (forward) and must form pairs with the same name (example: >Primer101_R ; >Primer101_F
-#PathToSamplesFolder="/mnt/shared/scratch/mc42302/201903_RTD2/Pacbio_20_samples/salmon_quants/quantsBaRT_1p_12/"
-#PathToSamplesFolder="/mnt/shared/scratch/mc42302/201903_RTD2/Pacbio_20_samples/salmon_quants/quantsBaRT_Illumina3_12/"
-#PathToSamplesFolder="/mnt/shared/scratch/mc42302/201903_RTD2/Pacbio_20_samples/salmon_quants/quantsBaRT_Iso4_12/"
-PathToSamplesFolder="/mnt/shared/scratch/mc42302/201903_RTD2/Pacbio_20_samples/salmon_quants/quantsBaRT_2_10_12/"
-
-#rtPCRInput="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/rtPCR_productsAndProportions_updated.txt"  ## ## Header: "Primer 	Size	INF2	LEAF	EMBRYO	INF1	NODE"
-#rtPCRInput="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/rtPCR_productsAndProportions_updated.txt"
-#rtPCRInput="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/rtPCR_productsAndProportions_updated.txt"
-rtPCRInput="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/rtPCR_productsAndProportions_updated.txt"
-#rtPCRInput="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/rtPCR_productsAndProportions_subset2.txt"
-scatterplot_output="/mnt/shared/scratch/mc42302/201903_RTD2/Benchmarking/RT_PCR/BaRT_2_10_12_w6"
+output="/path/to/output_prefix" #Output prefix
 
 
 conda activate blast
@@ -61,7 +49,7 @@ rm Transcriptome_DB.n*
 conda activate ggplot_python
 echo "Now analysing data..."
 
-python BaRT_RT_PCR_analysis.py -b $BlastN_Output -p $rtPCRInput -s $PathToSamplesFolder -w 6 -cw 0 -o $scatterplot_output
+python BaRT_RT_PCR_analysis.py -b $BlastN_Output -p $rtPCRInput -s $PathToSamplesFolder -w 6 -cw 0 -o $output
 
 conda deactivate
 
